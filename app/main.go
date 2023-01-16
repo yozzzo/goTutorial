96d2e8d8d8a4b4a4125debd284aa5c79c7fa8464
yozo <yozo@yozonoMacBook-Pro.local>
2023-01-16 12:56:28 +0900

    途中まで

--- app/main.go
+++ app/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+func outer() {
+	fmt.Println("outer")
+}
 func main() {
-	fmt.Println("Hello golang from docker!")
+	var i int = 100
+	var s string = "aaaaa"
+	fmt.Println(s)
+	fmt.Println(i)
+
+	var t, f bool = true, false
+	fmt.Println(t, f)
+
+	var (
+		i2 int    = 200
+		s2 string = "Golang"
+	)
+	fmt.Println(i2, s2)
+
+	var i3 int
+	var s3 string
+	fmt.Println(i3, s3)
+
+	i3 = 300
+	s3 = "Go"
+	fmt.Println(i3, s3)
+
+	i = 150
+	fmt.Println(i)
+
+	i4 := 400
+	fmt.Println(i4)
+
+	outer()
+	var s5 string = "Not Use"
+
 }
